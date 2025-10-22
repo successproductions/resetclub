987ee1b07a9ad1ebbc269a8206bab8089f641ce9
mohammed daali <w.master@successproductions.ma>
2025-10-22 16:02:56 +0100

    change margin of footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -186,7 +186,7 @@ const Footer: React.FC = () => {
 
         {/* Contact Information */}
         <div className="space-y-4 mb-8">
-          <div className="flex items-center justify-center text-gray-950">
+          <div className="flex items-center justify-center text-gray-950 mt-2">
             <svg className="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
             </svg>
