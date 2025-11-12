12e6c6506b3bcb04733be7a128cb210b8b888b9a
mohammed daali <w.master@successproductions.ma>
2025-11-12 09:37:21 +0100

    change text

--- src/components/sections/WhyResetClub.tsx
+++ src/components/sections/WhyResetClub.tsx
@@ -94,14 +94,14 @@ const WhyResetClub: React.FC = () => {
   return (
     <section className="md:pt-2 pt-4 bg-white">
       {/* Header Section */}
-      <div className="max-w-7xl mx-auto px-6 md:text-center mb-6 md:mb-16">
+      {/* <div className="max-w-7xl mx-auto px-6 md:text-center mb-6 md:mb-16">
         <h2 className="text-3xl md:text-4xl lg:text-5xl  text-gray-900 mb-1 ">
           {t('title')}
         </h2>
         <p className="text-lg md:text-lg xl:text-xl font-normal  font-graphik text-gray-900 ">
           {t('subtitle')}
         </p>
-      </div>
+      </div> */}
 
       {/* Cards Section - Full Width */}
       <div className="relative w-full overflow-hidden">
