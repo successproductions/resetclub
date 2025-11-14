83d4502caaeda9afda6e9b4aa70e1a1b9e01fdda
mohammed <w.master@successproductions.ma>
2025-11-14 11:59:19 +0100

    fix: enable looping for video in TeamHero component

--- src/components/sections/TeamHero.tsx
+++ src/components/sections/TeamHero.tsx
@@ -10,7 +10,7 @@ export default function TeamHero() {
         <video
           autoPlay
           muted
-          
+          loop
           playsInline
           className="w-full h-full object-cover"
         >
