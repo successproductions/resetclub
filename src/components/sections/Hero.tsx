70e3b97476570e471b62f4b2e1dfc01610146802
mohammed <w.master@successproductions.ma>
2026-06-12 11:55:57 +0100

    change images of hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -12,11 +12,10 @@ const Hero: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const images = [
-    '/images/image1.jpeg',
-    '/images/image2.jpeg',
-    '/images/image3.jpeg',
-    '/images/image4.jpeg',
-    '/images/image5.jpeg',
+    '/images/hero/hero1.jpeg',
+    '/images/hero/hero2.jpeg',
+    '/images/hero/hero3.jpeg',
+    '/images/hero/hero4.jpeg'
   ];
 
   const sliderTexts = [
@@ -67,15 +66,15 @@ const Hero: React.FC = () => {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 top-26 left-0 mx-2 max-w-4xl px-3 pt-4 md:left-24 lg:top-36 xl:top-0">
-        <div className="relative max-w-5xl bg-[linear-gradient(90deg,rgba(255,255,255,0.82)_0%,rgba(255,255,255,0.72)_42%,rgba(255,255,255,0)_100%)] px-5 py-5 md:px-4 md:py-7">
+      <div className="relative z-10 top-26 left-0 mx-2 w-[calc(100vw-2rem)] px-3 xl:px-1 pt-4 md:left-24 md:w-[calc(100vw-8rem)] lg:top-36 xl:top-0 xl:w-[min(88vw,2600px)]">
+        <div className="relative w-full px-5 xl:px-0 py-5 md:px-4 md:py-7">
 
           <h1 className="relative mb-4 font-graphik font-normal md:mb-6">
-            <div className="relative mb-2 min-h-[4rem] overflow-hidden text-3xl tracking-wide text-gray-900 md:mb-4 md:min-h-[3.5rem] md:text-xl lg:min-h-[2.5rem] lg:text-4xl">
+            <div className="relative mb-2 min-h-[2rem] overflow-hidden text-[18px] tracking-wide text-white md:mb-4 md:min-h-[3.5rem] md:text-2xl lg:min-h-[4.9rem] lg:text-6xl xl:text-7xl">
               {sliderTexts.map((text, index) => (
                 <span
                   key={text}
-                  className={`absolute left-0 top-0 transition-all duration-700 ${index === currentSlide
+                  className={`absolute left-0 top-0 whitespace-nowrap transition-all duration-700 ${index === currentSlide
                     ? 'translate-x-0 opacity-100'
                     : '-translate-x-8 opacity-0'
                     }`}
@@ -84,14 +83,14 @@ const Hero: React.FC = () => {
                 </span>
               ))}
             </div>
-            <div className="text-lg font-normal tracking-tight text-gray-900 md:text-xl xl:text-1xl">
+            <div className="text-lg font-normal tracking-tight text-white md:text-xl xl:text-1xl">
               {t('subtitle')}
             </div>
           </h1>
 
           {/* CTA Button */}
           <Link href="/payment" className="relative inline-block">
-            <button className="whitespace-nowrap border-2 bg-white/25 px-3 py-2 font-graphik text-lg font-normal text-black shadow-lg transition-all duration-300 hover:scale-105 hover:border-gray-200 hover:bg-gray-950 hover:text-white hover:shadow-xl md:px-5 md:text-xl">
+            <button className="whitespace-nowrap border-2  px-3 py-2 font-graphik text-lg font-normal text-white shadow-lg transition-all duration-300 hover:scale-105 hover:border-gray-200  hover:text-white hover:shadow-xl md:px-5 md:text-xl">
               {t('cta')}
             </button>
           </Link>
